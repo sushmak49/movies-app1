f6cdc8e854a109bfc2f24b7f2e4bd7cd15329180
sushmak49 <sushmak49@gmail.com>
2020-10-20 01:07:03 +0530

    Added poster,title,genre on Details Page

--- src/screens/details/Details.js
+++ src/screens/details/Details.js
@@ -1,33 +1,45 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Header from '../../common/header/Header.js'
 import moviesData from '../../common/moviesData.js';
+import Typography from '@material-ui/core/Typography';
+import '../details/Details.css';
 
-class Details extends Component{
+class Details extends Component {
 
-    constructor(){
+    constructor() {
         super();
         this.state = {
             movie: {}
         }
     }
 
-    componentDidMount() {
+    componentWillMount() {
         let currentState = this.state;
         currentState.movie = moviesData.filter((movie) => {
             return movie.id === this.props.movieId
         })[0];
-        this.setState({currentState});
+        this.setState({ currentState });
         console.log(this.state);
     }
     render() {
-        return(
+        let movie = this.state.movie;
+        return (
             <div className="details">
-            <Header />
-            <div className="flex-containerDetails">
-                <div className="leftDetails">left</div>
-                <div className="middleDetails">middle</div>
-                <div className="rightDetails">right</div>
-            </div>
+                <Header />
+                <div className="flex-containerDetails">
+                    <div className="leftDetails">
+                        <img src={movie.poster_url} alt={movie.title} />
+                    </div>
+                    <div className="middleDetails">
+                        <div>
+                            <Typography variant="headline" component="h2">{movie.title}</Typography>
+                        </div>
+                        <div>
+                            <Typography><span className="bold">Genre: </span>{movie.genres.join(', ')}</Typography>
+                        </div>
+                    </div>
+                    <div className="rightDetails">right</div>
+                </div>
             </div>
 
         )
